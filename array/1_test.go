77d380b4b14cf7def0f2f98fb5ffd474f3a7d31b
xz1220 <3024979463@qq.com>
2020-02-18 21:27:23 +0800

    readme

--- array/1_test.go
+++ array/1_test.go
@@ -3,11 +3,12 @@ package array
 import (
 	"fmt"
 	"testing"
+	"array"
 )
 
 func TestTwoSum(t *testing.T) {
 	nums := []int{2, 7, 11, 15}
 	target := 9
 
-	fmt.Println(TwoSum(nums, target))
+	fmt.Println(array.TwoSum(nums, target))
 }
