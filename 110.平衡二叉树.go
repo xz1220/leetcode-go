b218f6a182b69bc74fb8e7ceeaaabc00bd6ec119
xz1220 <3024979463@qq.com>
2020-03-28 18:41:20 +0800

    update

---
+++ 110.平衡二叉树.go
@@ -0,0 +1,98 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=golang
+ *
+ * [110] 平衡二叉树
+ *
+ * https://leetcode-cn.com/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (50.90%)
+ * Likes:    269
+ * Dislikes: 0
+ * Total Accepted:    61.5K
+ * Total Submissions: 120.7K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ * 
+ * 本题中，一棵高度平衡二叉树定义为：
+ * 
+ * 
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 给定二叉树 [3,9,20,null,null,15,7]
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 返回 true 。
+ * 
+ * 示例 2:
+ * 
+ * 给定二叉树 [1,2,2,3,3,null,null,4,4]
+ * 
+ * ⁠      1
+ * ⁠     / \
+ * ⁠    2   2
+ * ⁠   / \
+ * ⁠  3   3
+ * ⁠ / \
+ * ⁠4   4
+ * 
+ * 
+ * 返回 false 。
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isBalanced(root *TreeNode) bool {
+
+	flag:=true
+
+	var dfs func(*TreeNode)int
+
+	dfs=func(root *TreeNode)int{
+		if root==nil{
+			return 0
+		}
+
+		left:=dfs(root.Left)
+		right:=dfs(root.Right)
+
+		if left-right>1 || right-left>1{
+			flag=false
+		}
+
+		return max(left,right)+1
+	}
+
+	dfs(root)
+
+	return flag
+
+}
+
+func max(a,b int)int{
+	if a>b{
+		return a
+	}
+	return b
+}
+// @lc code=end
+
---
+++ 114.二叉树展开为链表.go
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=114 lang=golang
+ *
+ * [114] 二叉树展开为链表
+ *
+ * https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/description/
+ *
+ * algorithms
+ * Medium (68.09%)
+ * Likes:    298
+ * Dislikes: 0
+ * Total Accepted:    33K
+ * Total Submissions: 48.5K
+ * Testcase Example:  '[1,2,5,3,4,null,6]'
+ *
+ * 给定一个二叉树，原地将它展开为链表。
+ * 
+ * 例如，给定二叉树
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   5
+ * ⁠/ \   \
+ * 3   4   6
+ * 
+ * 将其展开为：
+ * 
+ * 1
+ * ⁠\
+ * ⁠ 2
+ * ⁠  \
+ * ⁠   3
+ * ⁠    \
+ * ⁠     4
+ * ⁠      \
+ * ⁠       5
+ * ⁠        \
+ * ⁠         6
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func flatten(root *TreeNode)  {
+
+}
+// @lc code=end
+
