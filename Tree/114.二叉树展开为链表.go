d0875f71fa6ac60e694e77f0b5d1ca9367efb7b5
xz1220 <3024979463@qq.com>
2020-08-05 10:37:35 +0800

    add a new solution for 114

--- Tree/114.二叉树展开为链表.go
+++ Tree/114.二叉树展开为链表.go
@@ -50,42 +50,66 @@
  */
 
  //前序便利(注意是要在原地便利，分情况讨论即可)
+// func flatten(root *TreeNode)  {
+
+// 	var dfs func(*TreeNode) *TreeNode
+
+// 	dfs=func(root *TreeNode) *TreeNode{
+
+// 		if root==nil{
+// 			return nil
+// 		}
+
+// 		if  root.Left==nil && root.Right==nil{
+// 			return root
+// 		}
+
+// 		if root.Left!=nil && root.Right==nil{
+// 			root.Right=root.Left
+// 			root.Left=nil
+// 			return dfs(root.Right)
+// 		}
+
+// 		if root.Left==nil && root.Right!=nil{
+// 			return dfs(root.Right)
+// 		}
+
+// 		if root.Left!=nil && root.Right!=nil{
+// 			left:=dfs(root.Left)
+// 			right:=dfs(root.Right)
+// 			left.Right=root.Right
+// 			root.Right=root.Left
+// 			root.Left=nil
+// 			return right
+// 		}
+// 		return nil
+// 	}
+
+// 	dfs(root)
+// }
+
+// 首先题目是前序遍历，结果是一个递增的数列，要求改为一个链表
+// 新思路： 按照前序遍历的顺序依次链接前驱和后继，并保证每次都在右孩子上
+//         前序遍历当前节点的右孩子的前驱 是 当前节点左孩子的最右孩子后者或者当前节点的左孩子
 func flatten(root *TreeNode)  {
+    curr := root
 
-	var dfs func(*TreeNode) *TreeNode
+    for curr != nil {
 
-	dfs=func(root *TreeNode) *TreeNode{
+        if curr.Left != nil{
+            next := curr.Left
+            preNode := next
 
-		if root==nil{
-			return nil
-		}
+            for preNode.Right != nil {
+                preNode = preNode.Right
+            } 
 
-		if  root.Left==nil && root.Right==nil{
-			return root
-		}
-
-		if root.Left!=nil && root.Right==nil{
-			root.Right=root.Left
-			root.Left=nil
-			return dfs(root.Right)
-		}
-
-		if root.Left==nil && root.Right!=nil{
-			return dfs(root.Right)
-		}
-
-		if root.Left!=nil && root.Right!=nil{
-			left:=dfs(root.Left)
-			right:=dfs(root.Right)
-			left.Right=root.Right
-			root.Right=root.Left
-			root.Left=nil
-			return right
-		}
-		return nil
-	}
-
-	dfs(root)
+            preNode.Right = curr.Right
+            curr.Right, curr.Left = next, nil
+        }
+        
+        curr = curr.Right
+    }
 }
 // @lc code=end
 
