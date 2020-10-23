f6c74bb7ea672b7b82a19910a8479188f9697937
xz1220 <3024979463@qq.com>
2020-10-23 18:47:23 +0800

    Sort: add the solution of 75

---
+++ sort/75.颜色分类.go
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=75 lang=golang
+ *
+ * [75] 颜色分类
+ *
+ * https://leetcode-cn.com/problems/sort-colors/description/
+ *
+ * algorithms
+ * Medium (55.09%)
+ * Likes:    685
+ * Dislikes: 0
+ * Total Accepted:    145.3K
+ * Total Submissions: 255.9K
+ * Testcase Example:  '[2,0,2,1,1,0]'
+ *
+ * 给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+ *
+ * 此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+ *
+ * 注意:
+ * 不能使用代码库中的排序函数来解决这道题。
+ *
+ * 示例:
+ *
+ * 输入: [2,0,2,1,1,0]
+ * 输出: [0,0,1,1,2,2]
+ *
+ * 进阶：
+ *
+ *
+ * 一个直观的解决方案是使用计数排序的两趟扫描算法。
+ * 首先，迭代计算出0、1 和 2 元素的个数，然后按照0、1、2的排序，重写当前数组。
+ * 你能想出一个仅使用常数空间的一趟扫描算法吗？
+ *
+ *
+ */
+
+// @lc code=start
+// 原地排序，主要使用两指针指向0和2应该交换的位置
+func sortColors(nums []int) {
+	p0, p2 := 0, len(nums)-1
+
+	for i := 0; i <= p2; i++ {
+		for ; i < p2 && nums[i] == 2; p2-- {
+			nums[i], nums[p2] = nums[p2], nums[i]
+		}
+		if nums[i] == 0 {
+			nums[i], nums[p0] = nums[p0], nums[i]
+			p0++
+		}
+	}
+}
+
+// @lc code=end
+
