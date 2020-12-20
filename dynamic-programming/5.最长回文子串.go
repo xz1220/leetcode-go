852cdf5216b4a2af11236dcad5e4cae85b9a1640
xz1220 <3024979463@qq.com>
2020-12-20 19:56:38 +0800

    Solution: add the effective solution of 5

--- dynamic-programming/5.最长回文子串.go
+++ dynamic-programming/5.最长回文子串.go
@@ -33,6 +33,7 @@
 // @lc code=start
 func longestPalindrome(s string) string {
 
+	// @lc author = CarryMeRookie
 	/*
 	 * 状态转移方程：
 	 * P(i,i) = true
@@ -41,35 +42,67 @@ func longestPalindrome(s string) string {
 	 *
 	 */
 
-	n := len(s)
-	answer := ""
-	dp := make([][]int, n)
-	for i := 0; i < n; i++ {
-		dp[i] = make([]int, n)
+	// n := len(s)
+	// answer := ""
+	// dp := make([][]int, n)
+	// for i := 0; i < n; i++ {
+	// 	dp[i] = make([]int, n)
+	// }
+
+	// for l := 0; l < n; l++ {
+	// 	for i := 0; i+l < n; i++ {
+	// 		j := i + l
+	// 		if l == 0 {
+	// 			dp[i][j] = 1
+	// 		} else if l == 1 {
+	// 			if s[i] == s[j] {
+	// 				dp[i][j] = 1
+	// 			}
+	// 		} else {
+	// 			if s[i] == s[j] {
+	// 				dp[i][j] = dp[i+1][j-1]
+	// 			}
+	// 		}
+
+	// 		if dp[i][j] > 0 && l+1 > len(answer) {
+	// 			answer = s[i : i+l+1]
+	// 		}
+	// 	}
+	// }
+	// return answer
+
+	// @lc author = CarryMeRookie
+
+	// don't forget to handle the bound
+	if s == "" {
+		return ""
 	}
 
-	for l := 0; l < n; l++ {
-		for i := 0; i+l < n; i++ {
-			j := i + l
-			if l == 0 {
-				dp[i][j] = 1
-			} else if l == 1 {
-				if s[i] == s[j] {
-					dp[i][j] = 1
-				}
-			} else {
-				if s[i] == s[j] {
-					dp[i][j] = dp[i+1][j-1]
-				}
-			}
+	// expandRange return the index of the longest suitable string from a specific start-point.
+	var expandRange func(string, int, int) (int, int)
+	expandRange = func(s string, left int, right int) (int, int) {
+		for ; left >= 0 && right < len(s) && s[left] == s[right]; left, right = left-1, right+1 {
+		}
+		return left + 1, right - 1
+	}
 
-			if dp[i][j] > 0 && l+1 > len(answer) {
-				answer = s[i : i+l+1]
-			}
+	// initiate the index
+	left, right := 0, 0
+	//
+	for i := 0; i < len(s); i++ {
+		left1, right2 := expandRange(s, i, i)
+		left2, right2 := expandRange(s, i, i+1)
+
+		if right1-left1 > right-left {
+			left, right = left1, right1
+		}
+
+		if right2-left2 > right-left {
+			left, right = left2, right2
 		}
 	}
-	return answer
 
+	return s[left : right+1]
 }
 
 // @lc code=end
