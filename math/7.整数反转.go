ce10c7c36b12f8d1390b219fb0fa20962704dae2
xz1220 <xingzheng@xingzhengdeMacBook-Pro.local>
2021-01-11 16:56:45 +0800

    Doc: update 7th solution

--- math/7.整数反转.go
+++ math/7.整数反转.go
@@ -5,16 +5,31 @@
  */
 
 // @lc code=start
+// func reverse(x int) int {
+// 	tmp := 0
+// 	for x != 0 {
+// 		tmp = tmp*10 + x%10
+// 		x = x / 10
+// 	}
+// 	if tmp > 1<<31-1 || tmp < -(1<<31) {
+// 		return 0
+// 	}
+// 	return tmp
+// }
+
 func reverse(x int) int {
-	tmp := 0
+	var res int
 	for x != 0 {
-		tmp = tmp*10 + x%10
+		if temp := int32(res); (temp*10)/10 != temp {
+			return 0
+		}
+		// if int32(res*10)/10 != int32(res) {
+		//     return 0
+		// }
+		res = res*10 + x%10
 		x = x / 10
 	}
-	if tmp > 1<<31-1 || tmp < -(1<<31) {
-		return 0
-	}
-	return tmp
+	return res
 }
 
 // @lc code=end
