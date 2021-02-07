32740dbfc8be2079fc1c7e300b95f99d5fee92ab
xz1220 <xingzheng@192.168.5.60>
2021-02-07 16:11:24 +0800

    Doc: update

---
+++ Array/16.最接近的三数之和.go
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=16 lang=golang
+ *
+ * [16] 最接近的三数之和
+ *
+ * https://leetcode-cn.com/problems/3sum-closest/description/
+ *
+ * algorithms
+ * Medium (45.86%)
+ * Likes:    681
+ * Dislikes: 0
+ * Total Accepted:    184.3K
+ * Total Submissions: 401.7K
+ * Testcase Example:  '[-1,2,1,-4]\n1'
+ *
+ * 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target
+ * 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 输入：nums = [-1,2,1,-4], target = 1
+ * 输出：2
+ * 解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 3 <= nums.length <= 10^3
+ * -10^3 <= nums[i] <= 10^3
+ * -10^4 <= target <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+func threeSumClosest(nums []int, target int) int {
+
+}
+// @lc code=end
+
