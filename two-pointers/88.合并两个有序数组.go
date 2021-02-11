75cf290ddd5a587e9c46506a17bcad4629383869
xz1220 <3024979463@qq.com>
2021-02-11 10:13:32 +0800

    add solution of 88

--- two-pointers/88.合并两个有序数组.go
+++ two-pointers/88.合并两个有序数组.go
@@ -50,6 +50,23 @@
 
 // @lc code=start
 func merge(nums1 []int, m int, nums2 []int, n int)  {
+    
+    for p:= m+n ; m>0 && n>0  ; p-- {
+        if nums1[m-1] > nums2[n-1] {
+            nums1[p-1] = nums1[m-1]
+            m = m -1
+        }else {
+            nums1[p-1] = nums2[n-1]
+            n = n -1
+        }
+    }
+
+    if m == 0 {
+        for n>0 {
+            nums1[n -1] = nums2[n -1]
+            n -- 
+        }
+    }
 
 }
 // @lc code=end
