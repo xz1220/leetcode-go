f347440f1b796fad3ee4a029bc47edc7c92c7ac7
xz1220 <3024979463@qq.com>
2021-02-12 16:53:38 +0800

    add solution of 167

--- two-pointers/167.两数之和-ii-输入有序数组.go
+++ two-pointers/167.两数之和-ii-输入有序数组.go
@@ -58,7 +58,16 @@
 
 // @lc code=start
 func twoSum(numbers []int, target int) []int {
-
+    for i,j := 0, len(numbers) -1; i<=j ; {
+        if numbers[i] + numbers[j] == target {
+            return []int{i+1, j+1}
+        }else if numbers[i] + numbers[j] < target {
+            i++
+        }else{
+            j--
+        }
+    }
+    return []int{}
 }
 // @lc code=end
 
