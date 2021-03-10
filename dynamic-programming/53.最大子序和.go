2d51f286e38ee53e5b67e451ddb06e7bb474c390
xz1220 <3024979463@qq.com>
2021-03-10 23:04:09 +0800

    Doc : add solution of 53

---
+++ dynamic-programming/53.最大子序和.go
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=golang
+ *
+ * [53] 最大子序和
+ *
+ * https://leetcode-cn.com/problems/maximum-subarray/description/
+ *
+ * algorithms
+ * Easy (52.17%)
+ * Likes:    2981
+ * Dislikes: 0
+ * Total Accepted:    436.9K
+ * Total Submissions: 816.5K
+ * Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
+ *
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出：6
+ * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [1]
+ * 输出：1
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：nums = [0]
+ * 输出：0
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：nums = [-1]
+ * 输出：-1
+ * 
+ * 
+ * 示例 5：
+ * 
+ * 
+ * 输入：nums = [-100000]
+ * 输出：-100000
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * -10^5 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的 分治法 求解。
+ * 
+ */
+
+// @lc code=start
+func maxSubArray(nums []int) int {
+    /* f(i) = max(f(i-1)+ nums[i], nums[i]) 
+     * f(i) 为以右端点为起始点的最大和连续子数组
+     *
+    */
+    var max int = nums[0]
+    for i:= 1; i < len(nums) ; i++ {
+        if nums[i-1] + nums[i] > nums[i] {
+            nums[i] += nums[i-1]
+        }
+
+        if nums[i] > max {
+            max = nums[i]
+        }
+    }
+
+    return max
+}
+// @lc code=end
+
