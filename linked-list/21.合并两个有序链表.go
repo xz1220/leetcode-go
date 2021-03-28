d71aab7d4da450c51423538d5f60b279e5d6d6da
xz1220 <3024979463@qq.com>
2021-03-28 16:17:17 +0800

    DOC: add the solution of 21

---
+++ linked-list/21.合并两个有序链表.go
@@ -0,0 +1,99 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=golang
+ *
+ * [21] 合并两个有序链表
+ *
+ * https://leetcode-cn.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (65.06%)
+ * Likes:    1629
+ * Dislikes: 0
+ * Total Accepted:    516.3K
+ * Total Submissions: 786.2K
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 两个链表的节点数目范围是 [0, 50]
+ * -100 
+ * l1 和 l2 均按 非递减顺序 排列
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+    if l1 == nil {
+        return l2
+    }
+    if l2 == nil {
+        return l1
+    }
+
+    dummyNode := &ListNode{0, l2}
+    pre := dummyNode
+    for l1 != nil {
+        if l2 == nil {
+            pre.Next = l1
+            break 
+        }
+
+        if l1.Val < l2.Val {
+            next := l1.Next
+            l1.Next = l2
+            pre.Next = l1
+            l1 = next
+            pre = pre.Next
+        }else {
+            l2 = l2.Next
+            pre = pre.Next
+        }
+    }
+    
+    return dummyNode.Next
+}
+// @lc code=end
+
