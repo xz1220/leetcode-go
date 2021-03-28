338430166b5870447d12f5f923b62ae85c265fa7
xz1220 <3024979463@qq.com>
2021-03-28 22:46:31 +0800

    DOC: add the solution of 17

---
+++ backtracking/17.电话号码的字母组合.go
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode.cn id=17 lang=golang
+ *
+ * [17] 电话号码的字母组合
+ *
+ * https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/description/
+ *
+ * algorithms
+ * Medium (55.66%)
+ * Likes:    1120
+ * Dislikes: 0
+ * Total Accepted:    218.3K
+ * Total Submissions: 391.3K
+ * Testcase Example:  '"23"'
+ *
+ * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+ * 
+ * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：digits = "23"
+ * 输出：["ad","ae","af","bd","be","bf","cd","ce","cf"]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：digits = ""
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：digits = "2"
+ * 输出：["a","b","c"]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 
+ * digits[i] 是范围 ['2', '9'] 的一个数字。
+ * 
+ * 
+ */
+
+// @lc code=start
+var phoneMap map[string]string = map[string]string{
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz",
+}
+
+var combinations []string
+
+func letterCombinations(digits string) []string {
+    if len(digits) == 0 {
+        return []string{}
+    }
+
+    combinations = []string{}
+    generate(digits, "")
+    return combinations
+}
+
+func generate(digits, str string) {
+    if len(digits) == 0 {
+        combinations = append(combinations, str)
+        return
+    }
+    digit := string(digits[0])
+    phoneStr := phoneMap[digit]
+    for i := range phoneStr {
+        generate(digits[1:], str + string(phoneStr[i]))
+    }
+}
+// @lc code=end
+
