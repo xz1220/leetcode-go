cf46deae97802c90715be2ada5fc12c767dc0e8e
xz1220 <3024979463@qq.com>
2021-04-23 00:39:56 +0800

    DOC: add solution of 25

--- linked-list/25.k-个一组翻转链表.go
+++ linked-list/25.k-个一组翻转链表.go
@@ -130,5 +130,71 @@
     }
     return dummmyNode.Next
 }
+
+func reverseKGroup(head *ListNode, k int) *ListNode {
+    dummyNode := &ListNode{0, head}
+    pre := dummyNode
+    for head != nil {
+        tail := pre
+        for i:= 0 ; i< k ; i++ {
+            tail = tail.Next
+            if tail == nil {
+                return dummyNode.Next
+            }
+        }
+        head, tail = reverse(head, tail)
+        pre.Next = head
+
+        pre = tail
+        head = pre.Next
+    }
+    return dummyNode.Next
+}
+
+func reverse( head ,end *ListNode) (*ListNode, *ListNode){
+    preV := end.Next
+    pre := head
+    for preV != end {
+        Next := pre.Next
+        pre.Next = preV
+        preV = pre
+        pre = Next
+    }
+    return end, head
+}
+
+// func reverseKGroup(head *ListNode, k int) *ListNode {
+//     hair := &ListNode{Next: head}
+//     pre := hair
+
+//     for head != nil {
+//         tail := pre
+//         for i := 0; i < k; i++ {
+//             tail = tail.Next
+//             if tail == nil {
+//                 return hair.Next
+//             }
+//         }
+//         // nex := tail.Next
+//         head, tail = myReverse(head, tail)
+//         pre.Next = head
+//         // tail.Next = nex
+//         pre = tail
+//         head = tail.Next
+//     }
+//     return hair.Next
+// }
+
+// func myReverse(head, tail *ListNode) (*ListNode, *ListNode) {
+//     prev := tail.Next
+//     p := head
+//     for prev != tail {
+//         nex := p.Next
+//         p.Next = prev
+//         prev = p
+//         p = nex
+//     }
+//     return tail, head
+// }
 // @lc code=end
 
